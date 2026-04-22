e81de70c0927f92ebf7caf6ba9a11cb576a5eaa4
BlaxkJesus <danieljames13182@gmail.com>
2026-04-22 09:23:40 +0100

    my main push

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,35 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/hello-wave';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={styles.titleContainer}>
+      <ThemedText type="title">Hello Ma</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
